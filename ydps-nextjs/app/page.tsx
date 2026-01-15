2493a1deb17ea49d23e429bb66cb223b7e98ac81
khata <hatakenaka@byui.edu>
2026-01-14 22:07:04 -0700

    Converted homepage to Next.js

---
+++ ydps-nextjs/app/page.tsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+  return (
+    <div id="content">
+      <header>
+        <a id="logo_link" href="/">
+          <img className="logo" src="images/ydps-logo-removedbg.png" alt="YDPS logo" />
+        </a>
+      </header>
+    </div>
+  )
+}
