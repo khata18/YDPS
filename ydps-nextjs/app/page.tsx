a777b2caa82d3eddf7f512a828424cd913ab54b2
khata <hatakenaka@byui.edu>
2026-01-19 21:49:44 -0700

    added header tsx

--- ydps-nextjs/app/page.tsx
+++ ydps-nextjs/app/page.tsx
@@ -3,8 +3,14 @@ export default function HomePage() {
     <div id="content">
       <header>
         <a id="logo_link" href="/">
-          <img className="logo" src="images/ydps-logo-removedbg.png" alt="YDPS logo" />
+          <img className="logo" src="/images/ydps-logo-removedbg.png" alt="YDPS logo" />
         </a>
+        <nav>
+          <a href="/services">Services</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact Us</a>
+          <a id="login" href="/login">Login</a>
+        </nav>
       </header>
     </div>
   )
